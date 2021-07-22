9b5923f00200cebcf1d929530a1a0a2d9520267f
stormzy22 <aawweb23@gmail.com>
2021-07-22 09:49:34 +0100

    Build a Music Player _ Vanilla JavaScript

---
+++ Build a Music Player _ Vanilla JavaScript/script.js
@@ -0,0 +1,93 @@
+const musicContainer = document.querySelector(".music-container");
+const playBtn = document.querySelector("#play");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const audio = document.querySelector("#audio");
+const progress = document.querySelector(".progress");
+const progressContainer = document.querySelector(".progress-container");
+const title = document.querySelector("#title");
+const cover = document.querySelector("#cover");
+
+// Song titles
+const songs = [
+  "Busta Rhymes - Touch It (TikTok Remix 2021)",
+  "Goosebumps (Remix)",
+  "Hillsong- Spirit lead me (Pro-Tee's Gqom Remake)",
+];
+
+//Keep track of songs
+let songIndex = 2;
+
+// Initially load song info DOM
+loadSong(songs[songIndex]);
+
+// Update the song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.png`;
+}
+
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+  audio.play();
+}
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) songIndex = songs.length - 1;
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) songIndex = 0;
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
+}
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  // console.log(width);
+  const clickX = e.offsetX;
+  console.log(clickX);
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+// Event listiner
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) return pauseSong();
+  playSong();
+});
+
+// Change song events
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+audio.addEventListener("timeupdate", updateProgress);
+progressContainer.addEventListener("click", setProgress);
+
+audio.addEventListener("ended", nextSong);
